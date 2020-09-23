7ed62f2234fd719f93786c0cccfcd8da7dd93151
caroldf07 <carolina.df@outlook.com>
2020-09-22 23:06:13 -0300

    DIO

---
+++ src/DIO/positiveNumbers.java
@@ -0,0 +1,44 @@
+package DIO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class positiveNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int a = scan.nextInt();
+
+        System.out.print("Input a number: ");
+        int b = scan.nextInt();
+
+        System.out.print("Input a number: ");
+        int c = scan.nextInt();
+
+        System.out.print("Input a number: ");
+        int d = scan.nextInt();
+
+        System.out.print("Input a number: ");
+        int e = scan.nextInt();
+
+        System.out.print("Input a number: ");
+        int f = scan.nextInt();
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(a);
+        numbers.add(b);
+        numbers.add(c);
+        numbers.add(d);
+        numbers.add(e);
+        numbers.add(f);
+
+        int positive = 0;
+        for (int number : numbers){
+            if (number > 0){
+                positive += 1;
+            }
+        }
+        System.out.println(positive+" valores positivos");
+    }
+}
